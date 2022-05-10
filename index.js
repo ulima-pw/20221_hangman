8e90fa9a9e923176d0d9727fc2be76d905123106
ProfL3502 <ProfL3502@lab.aca>
2022-05-10 13:16:15 -0500

    ahorcado terminado

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const refranes = [
 
 let refranSeleccionado = ""
 let refranOcultado = ""
+let numeroEquivocaciones = 0
+let terminado = false
 
 const seleccionarRefran = function() {
     // Seleccionar refran
@@ -36,14 +38,83 @@ const ocultarRefran = function() {
     document.getElementById("divRefran").innerText = refranOcultado
 }
 
-const buscarDescubrirCaracter = function() {
-    
+const buscarDescubrirCaracter = function(caracter) {
+    let refranTemp = ""
+    let encontrado = false
+    for (let i in refranSeleccionado) {
+        if (refranSeleccionado[i] == caracter) {
+            // Se debe descubrir
+            encontrado = true
+            refranTemp += refranSeleccionado[i]
+        }else {
+            refranTemp += refranOcultado[i]
+        }
+    }
+    refranOcultado = refranTemp
+    document.getElementById("inputLetras").value += caracter 
+    document.getElementById("divRefran").innerText = refranOcultado
+
+    return encontrado
+}
+
+const crearDivMensajeGanador = function() {
+    const div = document.createElement("div")
+    div.setAttribute("class", "alert alert-success")
+    div.setAttribute("color", "verde")
+    div.innerText = "Ganó"
+
+    return div
+}
+
+const crearDivMensajePerdedor = function() {
+    const div = document.createElement("div")
+    div.setAttribute("class", "alert alert-danger")
+    div.setAttribute("color", "rojo")
+    div.innerText = "Perdió"
+
+    return div
 }
 
 const onKeyPressed = function(event) {
-    const caracterSeleccionado = event.key
+    if (terminado) return
 
-    buscarDescubrirCaracter(caracterSeleccionado)
+    const caracterSeleccionado = event.key
+    
+    const encontrado = buscarDescubrirCaracter(
+        caracterSeleccionado.toUpperCase()
+    )
+
+    if (!encontrado) {
+        numeroEquivocaciones++
+        // Modificar la imagen
+        const imagen = document.getElementById("imagen")
+        imagen.setAttribute(
+            "src", 
+            `/imagenes/Hangman-${numeroEquivocaciones}.png`
+        )
+
+        if (numeroEquivocaciones == 6){
+            // Perdio
+            /*const mensajePerdedor = document.getElementById(
+                "mensajePerdedor")
+            mensajePerdedor.setAttribute("class", "alert alert-danger")*/
+            const div = crearDivMensajePerdedor()
+            const divPadre = document.getElementById("divMensajes")
+            divPadre.appendChild(div)
+            terminado = true
+        }
+    }else {
+        if (refranOcultado == refranSeleccionado){
+            // Gane
+            /*const mensajeGanador = document.getElementById(
+                "mensajeGanador")
+                mensajeGanador.setAttribute("class", "alert alert-success")*/
+            const div = crearDivMensajeGanador()
+            const divPadre = document.getElementById("divMensajes")
+            divPadre.appendChild(div)
+            terminado = true
+        }
+    }
 
 } 
 
